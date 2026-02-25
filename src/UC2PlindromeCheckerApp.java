3bf14757f31a61fda2963d0931add1a222ead20a
dipanshupoonia95211 <dipanshupoonia701@gmail.com>
2026-02-25 10:27:47 +0530

    UCx: [Description]

--- src/UC2PlindromeCheckerApp.java
+++ src/UC2PlindromeCheckerApp.java
@@ -1,18 +1,15 @@
-public class UC2PlindromeCheckerApp {
+public class palindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "madam";
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        if (isPalindrome) {
+        if (input.equals(reversed)) {
             System.out.println(input + " is a palindrome.");
         } else {
             System.out.println(input + " is not a palindrome.");
