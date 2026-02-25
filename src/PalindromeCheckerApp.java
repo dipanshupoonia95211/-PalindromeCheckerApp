51a728ef5de58fb9f2ddaf7f6b52380b8e552ec3
dipanshupoonia95211 <dipanshupoonia701@gmail.com>
2026-02-25 11:07:04 +0530

    Ignores case and spaces.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,22 +7,27 @@ public class PalindromeCheckerApp {
         System.out.println("Enter a string:");
         String input = sc.nextLine();
 
-        if (isPalindrome(input, 0, input.length() - 1)) {
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
             System.out.println("The given string is a Palindrome.");
         } else {
             System.out.println("The given string is not a Palindrome.");
         }
 
         sc.close();
-    }
-
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return isPalindrome(str, start + 1, end - 1);
     }
 }
